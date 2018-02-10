0d4858b16b5d58015bac88dcbcce2fc443fb9e65
Timothy Liu <tlkh.xms@gmail.com>
2018-02-10 04:55:33 -0800

    added nmap scan

--- passive_mapping.py
+++ passive_mapping.py
@@ -1,24 +1,41 @@
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
 from scapy.all import *
 
-addressList=[] # Will contain the list of found IP addresses
+from libnmap.process import NmapProcess
+from libnmap.parser import NmapParser
+
+addressList = [] # Will contain the list of found IP addresses
+
+def readPacket(pkt):
+        if pkt.haslayer(IP): # If our packet has an IP layer
+            IPaddress = pkt[IP].src # get IP address (located in IP layer)
+            MACaddress = pkt[Ether].src # get MAC address (located in Ether Layer)
+
+            if IPaddress not in addressList:    # Then If IP never found before
+                addressList.append(IPaddress) # Add to our list
+                print('\n[+] ' + str(pkt.summary()))
+                print ('[+] Host Found\nMAC: ' + MACaddress + ' | IP: '+ IPaddress) # Print characteristics
 
 def list(addressList): 
+    return readPacket(pkt)
 
-	def readPacket(pkt):
+while True:
+        try:
+                print('[+] Listening... \n')
+                sniff(prn=list(addressList)) # Sniff any type of packet, send each of them to readPacket()
+        except KeyboardInterrupt:
+                break
 
-		if pkt.haslayer(IP): # If our packet has an IP layer
-			IPaddress = pkt[IP].src # get IP address (located in IP layer)
-			MACaddress = pkt[Ether].src # get MAC address (located in Ether Layer)
-
-			if IPaddress not in addressList: 	# Then If IP never found before
-				addressList.append(IPaddress) # Add to our list
-				print('[+] ' + str(pkt.summary()))
-				print ('\n[+] Host Found\nMAC: ' + MACaddress + ' | IP: '+ IPaddress) # Print characteristics
-
-	return readPacket
-
-print('[+] Listening... \n')
-sniff(prn=list(addressList)) # Sniff any type of packet, send each of them to readPacket()
+for hostname in addressList:
+        nmproc = NmapProcess(hostname, "-sV")
+        rc = nmproc.run()
+        parsed = NmapParser.parse(nmproc.stdout)
+        host = parsed.hosts[0]
+        services = []
+        cracked = False
+        for serv in host.services:
+                services.append(str(serv.port) + "/" + str(serv.service))
+        
+        print(host)
+        print(services)
