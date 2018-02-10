d7a84f0d070fbbd29b5b3f84f15bc3a677ac2836
Timothy Liu <tlkh@users.noreply.github.com>
2018-02-10 17:20:06 +0800

    print packet

--- passive_mapping.py
+++ passive_mapping.py
@@ -3,21 +3,22 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 from scapy.all import *
 
-adressList=[] # Will contain the list of found IP addresses
+addressList=[] # Will contain the list of found IP addresses
 
-def list(adressList): 
+def list(addressList): 
 
 	def readPacket(pkt):
 
 		if pkt.haslayer(IP): # If our packet has an IP layer
-			IPadress = pkt[IP].src # get IP adress (located in IP layer)
-			MACadress = pkt[Ether].src # get MAC adress (located in Ether Layer)
+			IPaddress = pkt[IP].src # get IP address (located in IP layer)
+			MACaddress = pkt[Ether].src # get MAC address (located in Ether Layer)
 
-			if IPadress not in adressList: 	# Then If IP never found before
-				adressList.append(IPadress) # Add to our list
-				print ('[+] Host Found\nMAC: ' + MACadress + ' | IP: '+ IPadress + '\n') # Print caracteristics
+			if IPaddress not in addressList: 	# Then If IP never found before
+				addressList.append(IPaddress) # Add to our list
+				print(pkt)
+				print ('[+] Host Found\nMAC: ' + MACaddress + ' | IP: '+ IPaddress + '\n') # Print characteristics
 
 	return readPacket
 
 print('[+] Listening... \n')
-sniff(prn=list(adressList)) # Sniff any type of packet, send each of them to readPacket()
+sniff(prn=list(addressList)) # Sniff any type of packet, send each of them to readPacket()
