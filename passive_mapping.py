9773576753201f21ad361a1b380b4f76e4ad15fd
Timothy Liu <tlkh.xms@gmail.com>
2018-02-12 22:31:13 -0800

    working message queue + nmap scan

--- passive_mapping.py
+++ passive_mapping.py
@@ -1,39 +1,47 @@
-import logging
+import logging, pika, sys
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-from libnmap.process import NmapProcess
-from libnmap.parser import NmapParser
+def setup_message_queue(topic):
+        """Setup RabbitMQ messaging system"""
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue=topic)
+        channel.exchange_declare(exchange=topic+"_exchange",
+                                 exchange_type='topic')
+        print("[i] Running: message queue on: " + topic)
+        return True
 
-addressList = [] # Will contain the list of found IP addresses
+def publish(topic, message):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.basic_publish(exchange=topic+"_exchange",
+                      routing_key=topic,
+                      body=message)
+        print("[T] Sent message: " + message)
+        return True
 
-def readPacket(pkt):
+def read_packet(pkt):
         if pkt.haslayer(IP): # If our packet has an IP layer
-            IPaddress = pkt[IP].src # get IP address (located in IP layer)
+            ip_address = pkt[IP].src # get IP address (located in IP layer)
             MACaddress = pkt[Ether].src # get MAC address (located in Ether Layer)
 
-            if IPaddress not in addressList:    # Then If IP never found before
-                addressList.append(IPaddress) # Add to our list
+            if ip_address not in address_list:
+                address_list.append(ip_address)
                 print('\n[+] ' + str(pkt.summary()))
-                print ('[+] Host Found\nMAC: ' + MACaddress + ' | IP: '+ IPaddress) # Print characteristics
+                print('[+] Host Found\n[i] MAC: ' + MACaddress + ' | IP: '+ ip_address)
+                publish("ip_address", ip_address)
+                
+def list(address_list): 
+    return read_packet
 
-def list(addressList): 
-    return readPacket
+setup_message_queue("ip_address")
+address_list = []
 
-while True:
-        try:
-                print('[+] Listening... \n')
-                sniff(prn=list(addressList)) # Sniff any type of packet, send each of them to readPacket()
-        except:
-                for hostname in addressList:
-                        nmproc = NmapProcess(hostname, "-sV")
-                        rc = nmproc.run()
-                        parsed = NmapParser.parse(nmproc.stdout)
-                        host = parsed.hosts[0]
-                        services = []
-                        cracked = False
-                        for serv in host.services:
-                                services.append(str(serv.port) + "/" + str(serv.service))
-                        
-                        print(host)
-                        print(services)
+try:
+        print('[i] Listening... \n')
+        sniff(prn=list(address_list)) # Sniff any type of packet, send each of them to readPacket()
+except Exception as e:
+        print("\n[!] Error: " + str(e))
+        print("[i] Exiting...")
+        pass
